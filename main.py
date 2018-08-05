1c81158098bb1e6b78622501328dacfa593ff64e
CJ Turetzky <cturetzky@csumb.edu>
2018-08-05 19:53:01 +0000

    Finished A1Z26 Encryption

--- main.py
+++ main.py
@@ -1,27 +1,43 @@
 
 
 def encrypt_A1Z26(message):
+    encrypt_dict = {
+        'a': '1',
+        'b': '2',
+        'c': '3',
+        'd': '4',
+        'e': '5',
+        'f': '6',
+        'g': '7',
+        'h': '8',
+        'i': '9',
+        'j': '10',
+        'k': '11',
+        'l': '12',
+        'm': '13',
+        'n': '14',
+        'o': '15',
+        'p': '16',
+        'q': '17',
+        'r': '18',
+        's': '19',
+        't': '20',
+        'u': '21',
+        'v': '22',
+        'w': '23',
+        'x': '24',
+        'y': '25',
+        'z': '26',
+        ' ': '0',
+        ',': ',',
+        '.': '.',
+        '!': '!',
+        '?': '?',
+    }
     encrypted_str = ''
     for char in message:
-        if char.lower() == 'a':
-            encrypted_str += '1'
-        elif char.lower() == 'b':
-            encrypted_str += '2'
-        elif char.lower() == 'c':
-            encrypted_str += '3'
-        elif char.lower() == 'd':
-            encrypted_str += '4'
-        elif char.lower() == 'e':
-            encrypted_str += '5'
-        elif char.lower() == 'f':
-            encrypted_str += '6'
-        elif char.lower() == 'g':
-            encrypted_str += '7' #No idea why an error is showing up here, code runs fine.
-        elif char.lower() == 'h':
-            encrypted_str += '8'
-        else:
-            encrypted_str += char
-            
+        encrypted_str += encrypt_dict[char.lower()]
+        encrypted_str += '-'
     return encrypted_str
 
-print encrypt_A1Z26("gg nerd")
+print encrypt_A1Z26("Hello there!")
